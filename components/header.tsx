a33114bd2de27c8bce6129e7d0b2cb8633591a78
Minh Nguyễn <minhnguyen13864@gmail.com>
2023-12-14 01:40:57 +0700

    update cart and search icon

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,36 @@
 import React from "react";
-import { BiLogoFacebookCircle, BiLogoInstagram, BiPhone } from "react-icons/bi";
+import {
+  BiEnvelope,
+  BiLogoFacebookCircle,
+  BiPhone,
+  BiSearch,
+  BiShoppingBag,
+} from "react-icons/bi";
 
 export default function Header() {
   return (
-    <div className="">
-        <BiLogoFacebookCircle className="text-blue-500 text-6xl"/>
-        <BiLogoInstagram className="text-6xl text-rose-500"/>
-        <BiPhone className="text-6xl text-green-500"/>
+    <div className="bg-green-500 p-3">
+      <div className="flex w-full">
+        <div className="flex items-center mx-auto">
+          <div className="flex items-center">
+            <BiLogoFacebookCircle className="text-blue-500 text-2xl bg-white rounded-full" />
+            <p className="text-white mx-1 text-sm">Biệt Đội Báo Con</p>
+          </div>
+          <div className="flex items-center mx-10">
+            <BiEnvelope className="text-red-500 text-2xl bg-white rounded-md" />
+            <p className="text-white text-sm mx-1">bietdoibaocon@gmail.com</p>
+          </div>
+          <div className="flex items-center">
+            <BiPhone className="text-white text-2xl" />
+            <p className="text-white text-sm mx-1">0376342263</p>
+          </div>
+        </div>
+
+        <div className="flex items-center mx-auto">
+          <BiSearch className="text-2xl"/>
+          <BiShoppingBag className="text-2xl"/>
+        </div>
+      </div>
     </div>
   );
 }
