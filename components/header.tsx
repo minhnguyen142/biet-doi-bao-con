9cc327a31da1634c6cb24a33a4c3310344bbf316
Minh Nguyễn <minhnguyen13864@gmail.com>
2023-12-15 17:46:31 +0700

    update header

--- components/header.tsx
+++ components/header.tsx
@@ -1,36 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import {
-  BiEnvelope,
-  BiLogoFacebookCircle,
-  BiPhone,
-  BiSearch,
-  BiShoppingBag,
-} from "react-icons/bi";
+import { BiCartAdd, BiMapAlt, BiSearchAlt, BiUser } from "react-icons/bi";
 
 export default function Header() {
   return (
-    <div className="bg-green-500 p-3">
-      <div className="flex w-full">
-        <div className="flex items-center mx-auto">
-          <div className="flex items-center">
-            <BiLogoFacebookCircle className="text-blue-500 text-2xl bg-white rounded-full" />
-            <p className="text-white mx-1 text-sm">Biệt Đội Báo Con</p>
+    <main className="min-h-screen">
+      <title>Biệt Đội Báo Con</title>
+      <div className="bg-slate-100 w-full border-b-2 border-b-red-500 p-2">
+        <div className="text-2xl flex items-center">
+          <Link href="/" className="flex items-center text-green-500 font-bold">
+            <Image src="/4.png" width="50" height="50" alt="4"></Image>
+            <p className="hidden md:flex">Biệt Đội Báo Con</p>
+          </Link>
+         <div className="hidden md:flex-1 md:justify-end md:flex md:items-center">
+            <input
+              type="text"
+              name="Search"
+              placeholder="Tìm kiếm"
+              className="text-base rounded-md mx-2 pl-2 pr-6"
+            />
+            <Link href="">
+              <BiUser />
+            </Link>
+            <Link href="">
+              <BiCartAdd className="mx-6" />
+            </Link>
+            <Link href="">
+              <BiMapAlt />
+            </Link>
           </div>
-          <div className="flex items-center mx-10">
-            <BiEnvelope className="text-red-500 text-2xl bg-white rounded-md" />
-            <p className="text-white text-sm mx-1">bietdoibaocon@gmail.com</p>
-          </div>
-          <div className="flex items-center">
-            <BiPhone className="text-white text-2xl" />
-            <p className="text-white text-sm mx-1">0376342263</p>
-          </div>
-        </div>
-
-        <div className="flex items-center mx-auto">
-          <BiSearch className="text-2xl"/>
-          <BiShoppingBag className="text-2xl"/>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
