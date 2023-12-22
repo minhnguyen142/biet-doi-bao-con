f6048527d178a99bc7e915ee8eef0f804e41df2d
Minh Nguyễn <minhnguyen13864@gmail.com>
2023-12-22 23:43:42 +0700

    fix image on resizing not center

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import Image from "next/image";
 
-const MastHead: React.FC = () => {
+export default function MastHead() {
   return (
-    <div className="w-full h-full bg-green-500">
+    <div className="h-screen">
       <Image
         src="/xUvm1i5-christmas-trees-wallpaper.jpg"
         alt="xUvm1i5-christmas-trees-wallpaper"
-        height="1000"
-        width="1000"
-        className="rounded-b-md w-full h-full"
+        height="4000"
+        width="4000"
+        className="rounded-b-md w-full h-full object-cover"
       ></Image>
-      <p>Hello</p>
     </div>
   );
 };
-
-export default MastHead;
